95ba0948003364b36af7354cb5c776670eba2464
Raul Antonio Martinez Silgado <Raul Antonio Martinez Silgado>
2017-06-04 20:07:53 -0500

    Terminando de editar

--- AppVideo/www/js/app.js
+++ AppVideo/www/js/app.js
@@ -5,10 +5,21 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'LocalStorageModule'])
 
-angular.module('starter')
-    .controller('IndexController', ['localStorageService', '$scope', '$state', IndexController]);
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-.config(function ($stateProvider, $urlRouterProvider) {
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+
+  .config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('login', {
       url: '/login',
@@ -23,20 +34,4 @@ angular.module('starter')
     $urlRouterProvider.otherwise('/login'); //set default page
 });
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
 })
